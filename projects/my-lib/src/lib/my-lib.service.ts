aa22b4955c5649dbbada642462be1cf700995af3
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-27 23:04:26 +0900

    MyLibService に debug() を実装

--- projects/my-lib/src/lib/my-lib.service.ts
+++ projects/my-lib/src/lib/my-lib.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MyLibService {
+  constructor() {}
 
-  constructor() { }
+  debug() {
+    console.log('MyLibService - debug');
+  }
 }
